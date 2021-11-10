0f5c0089554000cec839e544a3cd5c879bb6657b
3imed-jaberi <imed_jebari@hotmail.fr>
2021-11-11 00:48:09 +0100

    add layouts --container 🧊 ..

---
+++ layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { NavBar } from '@components/NavBar'
+import { Footer } from '@components/Footer'
+
+export default function MainLayout({ children }) {
+  return (
+    <React.Fragment>
+      <NavBar />
+      {children}
+      <Footer />
+    </React.Fragment>
+  )
+}
